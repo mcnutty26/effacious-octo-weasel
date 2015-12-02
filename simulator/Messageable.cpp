ebcc307312bdcd7441bac77726a35d6fb79d2e01
Jon Gibson <JonGibson1993@gmail.com>
2015-12-02 23:02:42 +0000

    added a base class for everything that can send or recieve messages, also, slight redesign, talk about it tomorrow (thursday 3rd)

---
+++ simulator/Messageable.cpp
@@ -0,0 +1,28 @@
+#include "Messageable.hpp"
+
+Messageable::Messageable(CommMod cm)
+:communicationsModule(cm)
+{
+	messageFun = nullptr;
+}
+
+void Messageable::send_message(std::string contents)
+{
+	communicationsModule.push_out_message(contents);
+}
+
+std::string Messageable::wait_for_message()
+{
+	while(inQueue.empty()){}
+	std::string message = inQueue.front();
+	inQueue.pop();
+	return message;
+}
+
+void Messageable::push_message(std::string contents)
+{
+	if(!message_callback(contents))
+	{
+		inQueue.push(contents);
+	}
+}
