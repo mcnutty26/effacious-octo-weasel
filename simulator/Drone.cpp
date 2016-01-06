2fba08c7f466b73e65b1e1ed54e5f6722e6087ee
Jon Gibson <JonGibson1993@gmail.com>
2016-01-06 02:32:30 +0000

    work in progress on the two drone files, took some complex envelope-maths to work out some of it though...

--- simulator/Drone.cpp
+++ simulator/Drone.cpp
@@ -1,6 +1,58 @@
 #include "Drone.hpp"
 
-void Drone::send_message(std::string contents)
+#include <cmath>
+
+#define PI 3.14159265
+
+void Drone::upkeep()
 {
-	communicationsModule.outQueue.push(contents);
+	if(moveDR <= 0)
+	{
+		moveDR = 0;
+		return;
+	}
+	double radCon = PI/180;
+	double distance = moveDR < moveSpd? moveDR:moveSpd;
+
+	double xDistance = distance*sin(moveYAng*radCon);
+	double yDistance = distance*sin(moveZAngle*radCon);
+	double zDistance = distance*sin(moveXAngle*radCon);
+
+	xPos += xDistance;
+	yPos += yDistance;
+	zPos += zDistance;
+
+	moveDR -= distance;
 }
+
+void Drone::moveForward(double speed, double distance)
+{
+	moveAngle(speed, distance, xAng, yAng, zAng);
+}
+
+void Drone::moveUp(double speed, double distance)
+{
+	moveAngle(speed, distance, xAng, yAng, zAng);
+}
+
+void Drone::moveDown(double speed, double distance)
+{
+	moveAngle(speed, distance, xAng, yAng, zAng);
+}
+
+void Drone::moveLeft(double speed, double distance)
+{
+	moveAngle(speed, distance, xAng, yAng, zAng);
+}
+
+void Drone::moveRight(double speed, double distance)
+{
+	moveAngle(speed, distance, xAng, yAng, zAng);
+}
+
+void Drone::moveBack(double speed, double distance)
+{
+	moveAngle(speed, distance, xAng+180, yAng, zAng);
+}
+
+
--- simulator/Drone.hpp
+++ simulator/Drone.hpp
@@ -1,27 +1,43 @@
 #ifndef EOW_DRONE_HPP
 	#define EOW_DRONE_HPP
 
-	#include <functional>
 	#include <string>
 
+	#include "Messageable.hpp"
 	#include "CommMod.hpp"
 
 	class Drone
+	:protected Messageable
 	{
 		public:
-			Drone(std::function<void()> fun, CommMod cm);
-			void run();
-			void send_message(std::string contents);
-			std::string wait_for_message();
-			void message_callback(std::function<void()>)
+			Drone(CommMod* cm, double iX, double iY, double iZ, double maxSpeed);
 
-			double xPos, yPos, zPos;
-			double maxX, maxY, maxZ;
-			double xAng, yAng, zAng;
+			double xAng, yAng, zAng = 0;
+
+			virtual void droneFunction() = 0;
+			void upkeep();
+
+		protected:
+			void turn(double dXAngle, double dYAngle, double dZAngle);
+			//speed is in units/tick, with units being items on the ENV data map and a tick
+			//being however long a single cycle takes, should do some thread stuff to synch
+			//everything
+			void moveForward(double speed, double distance);
+			void moveUp(double speed, double distance);
+			void moveDown(double speed, double distance);
+			void moveLeft(double speed, double distance);
+			void moveRight(double speed, double distance);
+			void moveBack(double speed, double distance);
+
+			//in all cases, angles are measured as y0 being forward, x0 and z0 being level
+			//with the ground
+			void moveAngle(double speed, double distance, double xAngle, double yAngle, double zAngle);
+			double getMaxSpeed();
 
 		private:
-			std::function<void()> droneFun;
-			CommMod communicationsModule;
+			double maxSpeed;
+
+			double moveDR, moveSpd, moveXAng, moveYAng, moveZAng;
 	};
 
 #endif
