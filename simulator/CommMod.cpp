99fc6679fceb96d16a8871f2ee015e18c6b2a558
mcnutty26 <williamseymour123@googlemail.com>
2016-01-14 19:31:10 +0000

    fixed a typo and removed an old file

--- simulator/CommMod.cpp
+++ simulator/CommMod.cpp
@@ -15,7 +15,7 @@ void CommMod::push_in_message(std::string message)
 	inQueue.push(message);
 }
 
-void COmmMod::push_out_message(std::string message)
+void CommMod::push_out_message(std::string message)
 {
 	outQueue.push(message);
 }
