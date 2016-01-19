ad4115f2239832483937a3956e42d05214df2830
William Seymour <w.r.seymour@warwick.ac.uk>
2016-01-19 11:01:19 +0000

    gotta go to data mining ¬_¬

--- code/communication/aodv/Aodv_rrep.cpp
+++ code/communication/aodv/Aodv_rrep.cpp
@@ -3,7 +3,6 @@
 Aodv_rrep(int hop, int id, std::string src_ip, std::string dst_ip, int dst_seq, int life, int ttl){
 	using Aodv_message::Aodv_message(dest_ip, dest_seq, ttl);
 	hop_count = hop;
-	rreq_id = id;
 	source_ip = src_ip;
 	life_time = life;
 }
@@ -27,7 +26,6 @@ int get_life_time(){
 std::string serialize(){
 	std::string result = Aodv_message::serialze();
 	result = "RREP;" + result;
-	result += ";" + std::to_string(hop_count) + ";" + std::to_string(rreq_id) + ";" 
-		+ source_ip + ";" + std::to_string(life_time);
+	result += ";" + std::to_string(hop_count) + ";" + source_ip + ";" + std::to_string(life_time);
 	return result;
 }
