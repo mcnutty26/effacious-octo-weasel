322b36e280dc70d619e54c83bf656b5a51fdd2ab
William Seymour <w.r.seymour@warwick.ac.uk>
2016-04-02 14:09:10 +0100

    added automated setup script for pi nodes

---
+++ code/liboctodroneparrot/config/setup.sh
@@ -0,0 +1,8 @@
+apt-get install -y vim git node
+git clone https://github.com/mcnutty26/effacious-octo-weasel.git
+cd effacious-octo-weasel/code/liboctodrone
+make
+cd ../liboctodronecomms
+make
+cd ../liboctodroneparrot
+make
