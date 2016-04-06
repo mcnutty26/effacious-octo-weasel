fc90b800d4aee7ca76bf9a04979b404f220a5bbb
mcnutty26 <williamseymour123@googlemail.com>
2016-04-06 22:31:25 +0100

    Added license headers to all source files

--- code/liboctodrone/include/Drone.hpp
+++ code/liboctodrone/include/Drone.hpp
@@ -1,3 +1,20 @@
+/*
+This file is part of octoDrone.
+
+octoDrone is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+octoDrone is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with octoDrone.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef EOW_DRONE_HPP
 	#define EOW_DRONE_HPP
 
