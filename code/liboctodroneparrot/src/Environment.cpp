ab64893e7e2428aa648efdebac384267380f5ae7
William Seymour <w.r.seymour@warwick.ac.uk>
2016-04-08 22:51:42 +0100

    more fixes

--- code/liboctodroneparrot/src/Environment.cpp
+++ code/liboctodroneparrot/src/Environment.cpp
@@ -55,7 +55,7 @@ void startNode(){
 		std::cout << "error@nodeServer: starting process" << std::endl;
 		exit(1);
 	}
-	std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+	std::this_thread::sleep_for(std::chrono::milliseconds(5000));
 }
 
 Drone* Environment::getDrone(){
